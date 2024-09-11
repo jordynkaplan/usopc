de5536f26b58772efcd5d977ff33b6b4f39eb5f1
jordynkaplan <105733892+jordynkaplan@users.noreply.github.com>
2024-09-10 21:08:28 -0400

    Update header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
 import { Individual } from "./pages/Individual";
 import { Header } from "./pages/Header";
 
--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,15 +1,15 @@
 import { Button } from "@/components/ui/button";
-import { Triangle } from "lucide-react";
 
 export function Header() {
   return (
-    <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-            <div className="p-2">
-              <Button variant="ghost" size="icon" aria-label="Home">
-                <img src="/USOPCLogo.svg"></img>
-                <Triangle className="size-5 fill-foreground" />
-              </Button>
-            </div>
-          </nav>
+    <header className="sticky top-0 z-10 flex h-[57px] items-center border-b bg-accent px-5">
+      <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+        <div className="p-2">
+          <Button variant="ghost" aria-label="Home">
+            <img className="h-full" src="/USOPCLogo.svg"></img>
+          </Button>
+        </div>
+      </nav>
+    </header>
   );
 }
