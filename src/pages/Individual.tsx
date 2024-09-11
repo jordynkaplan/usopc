25510786ada59c3678ea9c3cd4070faa6b70e16f
jordynkaplan <105733892+jordynkaplan@users.noreply.github.com>
2024-09-10 21:10:59 -0400

    Add demo chart

--- src/pages/Individual.tsx
+++ src/pages/Individual.tsx
@@ -1,7 +1,13 @@
+import { DemoChart } from "@/components/demo-chart";
 import { useResultsData } from "@/lib/data";
 
 export function Individual() {
   const data = useResultsData();
   console.log({ data: data });
-  return <>Individual</>;
+  return (
+    <>
+      Individual
+      <DemoChart />
+    </>
+  );
 }
